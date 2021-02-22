407908d81576d730ff85957062744a1e1f6336aa
Sagadat Yesbolsynov <190103129@stu.sdu.edu.kz>
2021-02-22 15:39:08 +0600

    first commit

---
+++ resources/views/contact_page.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align:center;">This is the page with contact information</h1>
+    <div style="height:300px;width:400px;border:2px solid  #3d6882;
+    border-radius: 10%;background-color: #feffea;margin-left:550px;">
+        <h2 style="text-align:center;">Feel free to ask</h2>
+        <p style="margin-left: 50px;font-size:25px;">Phone: 8 700 997 9389 <br>
+       Email: 190103129@sdu.edu.kz
+    </p>
+    </div>
+</body>
+</html></html>
---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ asset('cv.css') }}"/>
+    <title>Saga CV</title>
+</head>
+<body background="https://images.pexels.com/photos/4245826/pexels-photo-4245826.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+    <div class="saga">
+        <p style="margin-top: 180px;" id="pees">S-skillfull</p>
+        <p id="pees">A-accurate</p>
+        <p id="pees">G-goal oriented</p>
+        <p id="pees">A-attentive</p>
+        <p id="pees">D-disciplined</p>
+        <p id="pees">A-articulate</p>
+        <p id="pees">T-trustworthy</p>
+    </div>
+
+    <div class="saga-top">
+        <h1 style="padding-top: 25px;">Sagadat Yesbolsynov</h1>
+    </div>
+
+    <div class="mix">
+        <img id="logo" src="https://img.pngio.com/symbol-computer-icons-letter-font-letter-s-16001600-transprent-png-free-download-angle-area-text-s-png-1600_1600.png" alt="logo" width="120px">
+    </div>
+
+    <div class="card">
+        <img src="{{ asset('card-photo.jpg.JPG') }}" class="card-img-top" alt="photo">
+        <div class="card-body"></div>
+    </div>
+
+    <svg height="200" width="15rem">
+        <polyline color="white" points="500,300 800,280 280,520"/>
+    </svg>
+
+    <div class="personal-info">
+        <h3 style="margin-top: 10px; margin-left: 20px;">Personal Information</h3>
+        <p style="margin-left: 10px;">Name: Sagadat Yesbolsynov <br>
+           Age: 19 <br> 
+           Hometown: Taldykorgan <br>
+           Nationality: Kazakh <br>
+        </p>
+    </div>
+
+  <div class="edu-info">
+    <h3 style="margin-top: 10px; text-align: center;">Education</h3>
+    <p style="margin-left: 10px;">
+       2011: Finished Primary school at Taldykorgan N25<br>
+       2017: Finished Junior Highschool at Taldykorgan NIS<br> 
+       2019: Finished Senior Highschool at Taldykorgan NIS<br>
+       2019-2023: Studying at SDU<br>
+    </p>
+</div>
+
+<div class="work-info">
+    <h3 style="margin-top: 10px; text-align: center;">Working Experience</h3>
+    <p style="margin-left: 10px;">2014: Google Headquarters, California USA <br>
+       2017: Apple Park, Corporate Headquarters<br> 
+       2019: Tesla Motors, California USA<br>
+       2020: Nasa-SpaceX, Future Spaceprogramme, ISS <br>
+    </p>
+</div>
+
+<div class="contact-info">
+    <h3 style="margin-top: 10px; text-align: center;">Contact Information</h3>
+    <p style="margin-left: 10px;">Phone: 8 700 997 9389 <br>
+       Email 1: 190103129@sdu.edu.kz <br> 
+       Email 2: esbolsynov.s@gmail.com <br>
+       Office phone: 8 7282 222 33 44<br>
+    </p>
+</div>
+</body>
+</html>
